611c038247b10e1221f2abb9a495cce9a2e6b9ce
Daniel GIT DR2010 <daniellgmachado@gmail.com>
2018-07-18 09:40:55 +1000

    jul 2018

--- gitit.cmd
+++
@@ -1,6 +0,0 @@
-echo "# ristorante" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/DR2010/ristorante.git
-git push -u origin master
